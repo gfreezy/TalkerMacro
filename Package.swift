569b118ac0ace6f9c45c7c2ff16813220c6c68f4
Alex.F <gfreezy@gmail.com>
2025-05-30 21:19:31 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-syntax.git", from: "600.0.1")
+        .package(url: "https://github.com/swiftlang/swift-syntax", from: "600.0.1")
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
